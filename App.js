1650078ac663be616eb7831ff9385fb6a6cecf4c
Shivam Pandey <54200130+ShivamPandey001@users.noreply.github.com>
2022-11-29 10:06:50 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,4 +1,32 @@
-        // Hello world using React
-        const heading = React.createElement("h1",{id:"title"},"Hello world!");
-        const root = ReactDOM.createRoot(document.getElementById("root"));
-        root.render(heading);
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// const heading = React.createElement("div",{id:"title"},[
+//     React.createElement("h1",{id:"heading"}, "NamasteReact"),
+//     React.createElement("h2",{}, "From zero to Hero"),
+//     React.createElement("h3",{}, "in 3 months"),
+// ]);
+
+// const noOfMonths =3;
+// const heading = (
+//     <div>
+//         <h1>Namaste React</h1>
+//         <h2>from zero to Hero!</h2>
+//         <h3>in {noOfMonths} months</h3>
+//     </div>
+// );
+const TitleComponent = () => <h1> Namaste React Bootcamp</h1>
+
+const HeadingComponent = () => {
+    return (
+            <div id="title" className='=title-class'>
+                <TitleComponent />
+                <h2>from zero to Hero!</h2>
+                <h3>in 3 months</h3>
+            </div>
+        );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<HeadingComponent />);
